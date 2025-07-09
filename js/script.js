7e85e363fe4f4405229cfcd141746bc1155ee39b
Yarong Li <lyr2451536573@gmail.com>
2025-07-08 22:30:21 -0700

    Base code for a website

---
+++ js/script.js
@@ -0,0 +1,35 @@
+document.querySelector(".icon-menu").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.body.classList.toggle("menu-open");
+});
+
+const spollerButtons = document.querySelectorAll("[data-spoller] .spollers-faq__button");
+
+spollerButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const currentItem = button.closest("[data-spoller]");
+    const content = currentItem.querySelector(".spollers-faq__text");
+
+    const parent = currentItem.parentNode;
+    const isOneSpoller = parent.hasAttribute("data-one-spoller");
+
+    if (isOneSpoller) {
+      const allItems = parent.querySelectorAll("[data-spoller]");
+      allItems.forEach((item) => {
+        if (item !== currentItem) {
+          const otherContent = item.querySelector(".spollers-faq__text");
+          item.classList.remove("active");
+          otherContent.style.maxHeight = null;
+        }
+      });
+    }
+
+    if (currentItem.classList.contains("active")) {
+      currentItem.classList.remove("active");
+      content.style.maxHeight = null;
+    } else {
+      currentItem.classList.add("active");
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
